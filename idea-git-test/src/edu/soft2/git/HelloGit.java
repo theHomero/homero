08c4f3d4981427772cee7208cae84d0fbb014856
theHomero <2539177068@qq.com>
2021-12-05 13:52:52 +0800

    添加test工程至git

---
+++ idea-git-test/src/edu/soft2/git/HelloGit.java
@@ -0,0 +1,11 @@
+package edu.soft2.git;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
