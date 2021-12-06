e48f0e53ce5d30b44790e4faa0b8ee88a12fc269
theHomero <2539177068@qq.com>
2021-12-06 18:45:48 +0800

    添加HiGit.java

---
+++ idea-git-test/src/edu/soft2/git/HiGit.java
@@ -0,0 +1,11 @@
+package edu.soft2.git;
+
+public class HiGit {
+    public static void main(String[] args) {
+        System.out.println("hi git!");
+        System.out.println("hi git!");
+        System.out.println("hi git!");
+        System.out.println("hi git!");
+        System.out.println("hi git!");
+    }
+}
